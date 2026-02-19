0831e2a68ea8822ea31a6cb7a495c586ed9e287e
hyeongu.jo <hg.jo@gsretail.com>
2026-02-19 14:17:31 +0900

    [feature] Add Android repaint method

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/InAppWebViewMethodHandler.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/InAppWebViewMethodHandler.java
@@ -2,6 +2,7 @@ package com.pichillilorenzo.flutter_inappwebview;
 
 import android.net.Uri;
 import android.os.Build;
+import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 
